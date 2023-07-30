67fcf132030d39d0062d4eec42d505a7ae25da27
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-07-30 17:45:02 +0330

    Add files via upload

---
+++ ConsoleApp31/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    public class Time1
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+
+
+        public void Settime(int hour, int minute, int second)
+        {
+            if ((hour < 0 || hour > 23) || (minute < 0 || minute > 59) || (second < 0 || second > 59))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+
+            Hour = hour;
+            Minute = minute;
+            Second = second;
+        }
+
+        public string ToUniversalString() => $"{Hour:D2}:{Minute:D2}:{Second:D2}";
+
+        public override string ToString() => $"{((Hour == 0 || Hour == 12) ? 12 : Hour % 12)}:" + $"{Minute:D2}:{Second:D2}{(Hour < 12 ? "AM" : "PM")}";
+         }
+}
+
+
+    
+
---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var time = new Time1();
+
+            Console.WriteLine($"The initial universal time is:{time.ToUniversalString()}");
+            Console.WriteLine($"The initial standard time is:{time.ToString()}");
+            Console.WriteLine();
+
+            time.Settime(13, 27, 6);
+
+            Console.WriteLine($"Universal time after SetTime is:{time.ToUniversalString()}");
+            Console.WriteLine($"Standard time after SetTime is:{time.ToString()}");
+
+            try
+            {
+                time.Settime(99, 99, 99);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message + "\n");
+
+            }
+
+            Console.WriteLine("After attempting invalid setting:");
+            Console.WriteLine($"Universal time:{time.ToUniversalString()}");
+            Console.WriteLine($"Standard time:{time.ToString()}");
+            Console.ReadLine();
+        }
+    }
+}
